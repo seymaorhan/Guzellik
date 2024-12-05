bac1bc21d57c20c89ebf628c289f121663ec35ca
şeyma orhan <seymaorhan1453@hotmail.com>
2024-12-05 16:35:36 +0300

    Hakkımızda ve Hizmetlerimiz sayfalarını yaptım

---
+++ Guzellik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Guzellik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
